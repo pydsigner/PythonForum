c484a5e6be8e37d970eaa5033cc7bab9125b2f4f
Jakob Bowyer <jkbbwr@gmail.com>
2012-10-15 19:37:43 +0100

    Pushing .gitignore and fixed db_config.py

--- PythonForum/database/__init__.py
+++ PythonForum/database/__init__.py
@@ -5,7 +5,7 @@ from mongoengine import connect
 # For security reasons the server location isn't pushed with the project.
 # Try and import that location here
 try:
-    from db_config import db_uri as host
+    from PythonForum.db_config import db_uri as host
 except ImportError:
     # On failing to import that information look for an environ variable to tell us where to connect
     # Failing that just try for a local host connection
